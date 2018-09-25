7a7d93dae7c66181133c441cea61466d88733c9b
Mathis Paturle <mathipaturle@enti.cat>
2018-09-25 22:48:36 +0200

    Yupi Esta Nadando

--- Oktopuss/Assets/player.cs
+++ Oktopuss/Assets/player.cs
@@ -3,6 +3,8 @@
 public class player : MonoBehaviour {
 
 	private Rigidbody rb;
+	public float maxSpeed = 5.0f;
+	float speed = 8;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +13,18 @@ public class player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), Mathf.Clamp(rb.velocity.y, -maxSpeed, maxSpeed));
+
+		inputHandler ();
+	}
+
+	void inputHandler(){
+
+		float h = speed * Input.GetAxis("Horizontal");
+		float v = speed * Input.GetAxis("Vertical");
+
+		if (Input.GetButtonDown("Fire3")) {
+			rb.AddForce( new Vector3 (h * speed, v * speed, 0));
+		}
 	}
 }
