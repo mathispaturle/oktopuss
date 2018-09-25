b724765c277fc8546bc87fde0703eede40469df6
Mathis Paturle <mathipaturle@enti.cat>
2018-09-25 23:54:46 +0200

    Core game

--- Oktopuss/Assets/player.cs
+++ Oktopuss/Assets/player.cs
@@ -5,6 +5,8 @@ public class player : MonoBehaviour {
 	private Rigidbody rb;
 	public float maxSpeed = 5.0f;
 	float speed = 8;
+	bool hasTreasure = false;
+	int score = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -23,8 +25,25 @@ public class player : MonoBehaviour {
 		float h = speed * Input.GetAxis("Horizontal");
 		float v = speed * Input.GetAxis("Vertical");
 
-		if (Input.GetButtonDown("Fire3")) {
+		if (Input/*.GetButtonDown("Fire3")*/.GetKeyDown("space")) {
 			rb.AddForce( new Vector3 (h * speed, v * speed, 0));
 		}
+	}
+
+	void OnTriggerEnter(Collider col){
+
+		if (col.gameObject.tag == "reward") {
+			hasTreasure = true;
+			print ("Player took treasure");
+		}
+
+		if (col.gameObject.tag == "target") {
+			if (hasTreasure) {
+				print ("Player released treasure: " + score);
+				score++;
+				hasTreasure = false;
+			}
+		}
+
 	}
 }
---
+++ Oktopuss/Assets/sineMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sineMovement : MonoBehaviour {
+
+	private Vector3 _startPosition;
+
+	// Use this for initialization
+	void Start () {
+		_startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = _startPosition + new Vector3(0.0f, Mathf.Sin(Time.time)/4-0.5f, 0.0f);
+	}
+}
