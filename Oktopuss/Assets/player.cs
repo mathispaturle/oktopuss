867f7fb2314bacda589d57f18dfcb40c5149052f
Mathis Paturle <mathipaturle@enti.cat>
2018-09-25 22:09:37 +0200

    Yay

---
+++ Oktopuss/Assets/player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
